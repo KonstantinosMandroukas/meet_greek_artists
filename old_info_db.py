9f70d2f89d2803d2447ed287f5dce017093d6ad5
Konstantinos Mandroukas <konmanfts@gmail.com>
2026-01-10 10:02:32 +0200

    Yes commit

--- old_info_db.py
+++ old_info_db.py
@@ -1,8 +1,8 @@
-# import mysql.connector
+import mysql.connector
 
-# mydb = mysql.connector.connect(  
-# 	host = "localhost",
-# 	user = "root",
-# 	password = "admin1234",
-#     database = "meet_greek_artists"
-# ) # noqa: F821
+mydb = mysql.connector.connect(  
+	host = "localhost",
+	user = "root",
+	password = "admin1234",
+    database = "meet_greek_artists"
+) 
